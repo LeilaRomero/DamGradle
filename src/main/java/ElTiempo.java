e56014b4286499df0e5d46aa0708d7cf67f28913
Leila Romero Fajar <lromerofajar@danielcastelao.org>
2020-02-05 14:48:07 +0100

    insertar los grados que hacen arreglado

--- src/main/java/ElTiempo.java
+++ src/main/java/ElTiempo.java
@@ -1,6 +1,7 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 import java.io.IOException;
@@ -9,9 +10,9 @@ public class ElTiempo {
     public static void main(String[]args) throws IOException {
 
         Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").userAgent("Mozilla").get();
-        Element miElemento= doc.getElementsByClass("div class=today_nowcard-temp").first();
-       // Element misElemento= doc.getElementsByClass("div class=today_nowcard-phrase").first();
-
+        Elements miElemento= doc.getElementsByClass("today_nowcard-temp");
+        //Element misElemento= doc.getElementsByClass("div class=today_nowcard-phrase").first();
+        String miTexto=miElemento.text();
                 System.out.println(miElemento);
     }
 
