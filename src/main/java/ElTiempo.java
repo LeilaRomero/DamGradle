c6abb6584e7f0fcae7c6b7ccb2c95a5dad218e93
Leila Romero Fajar <lromerofajar@danielcastelao.org>
2020-02-05 14:31:32 +0100

    Insertar página web

---
+++ src/main/java/ElTiempo.java
@@ -0,0 +1,17 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+
+import java.io.IOException;
+
+public class ElTiempo {
+    public static void main(String[]args) throws IOException {
+
+        Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").userAgent("Mozilla").get();
+        Element miElemento= doc.select("div").first();
+        String miTexto=miElemento.text();
+                System.out.println(miTexto);
+    }
+
+}
