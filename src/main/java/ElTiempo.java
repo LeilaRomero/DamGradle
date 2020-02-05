a07a9abc244655c8ff3af01e0b9700edb3e431f2
Leila Romero Fajar <lromerofajar@danielcastelao.org>
2020-02-05 14:46:24 +0100

    insertar los grados que hacen

--- src/main/java/ElTiempo.java
+++ src/main/java/ElTiempo.java
@@ -9,9 +9,10 @@ public class ElTiempo {
     public static void main(String[]args) throws IOException {
 
         Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").userAgent("Mozilla").get();
-        Element miElemento= doc.select("div").first();
-        String miTexto=miElemento.text();
-                System.out.println(miTexto);
+        Element miElemento= doc.getElementsByClass("div class=today_nowcard-temp").first();
+       // Element misElemento= doc.getElementsByClass("div class=today_nowcard-phrase").first();
+
+                System.out.println(miElemento);
     }
 
 }
